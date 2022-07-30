fb0b870a35e017003a3580848de1188cf75e3d4f
sahilspst <spstheader@gmail.com>
2022-07-30 12:19:39 +0530

    To understand friend class

--- test3.cpp
+++ test3.cpp
@@ -1,47 +1,31 @@
 #include <iostream>
 using namespace std;
 
-class node
-{
-    int data;
-    human a;
-public:
-
-};
-
+class node;
 
 class human
 {
-    int data;
-
+    int a;
+    int b;
+    
 public:
-
+    int func(node, node);
+    
 };
 
-
-
-void printTree(node *ptr)
+class node
 {
-    if (!ptr)
-    {
-        return;
-    }
-    printTree(ptr->left);
-    cout << ptr->data;
-    printTree(ptr->right);
-    return;
+    int var1;
+    friend int human::func(node, node);
+
+public:
+};
+
+int human::func(node a, node b){
+    return a.var1 + b.var1;
 }
 
 int main()
 {
-    node *head = new node(0);
-    head->left = new node(1);
-    head->right = new node(2);
-    head->left->left = new node(3);
-    head->left->right = new node(4);
-    head->right->left = new node(5);
-    head->right->right = new node(6);
-    printTree(head);
-
     return 0;
 }
